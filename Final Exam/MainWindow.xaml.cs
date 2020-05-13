5572d82abe6b2d3cd71aba79185d94a741cab844
Christopher Sullivan <S00188845@mail.itsligo.ie>
2020-05-13 11:41:52 +0100

    Part 4

--- Final Exam/MainWindow.xaml.cs
+++ Final Exam/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,11 +21,55 @@ namespace Final_Exam
     /// </summary>
     public partial class MainWindow : Window
     {
+        PhoneData db = new PhoneData();
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Display phone in list box
+            var query = from a in db.Phones
+                        orderby a.Name
+                        select a.Name;
+                        
+
+            lstbxPhone.ItemsSource = query.ToList();
+        }
+
+        private void lstbxPhone_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+
+            
+
+            // display phone image
+            Phone selectedphone = lstbxPhone.SelectedItem as Phone;
+
+            if(selectedphone != null)
+            {
+                var query = from a in db.Phones
+                            where a.Name.Equals(selectedphone)
+                            select new PhonePrice
+                            {
+                                Price = a.Price
+                            };
+
+                //Pricetxtblk.Text.Equals(PhonePrice)
+
+                //show image
+                Phoneimg.Source = new BitmapImage(new Uri($"/images/{selectedphone.PhoneImage}"));
+                            
+            }
+
+        }
+        public class PhonePrice : Phone
+        {
+            public override string ToString()
+            {
+                return string.Format("Price : {0}",Price);
+            }
+        }
     }
 }
