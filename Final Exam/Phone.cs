188f21914c4a183c0b4ba3171a82b04b9422bd6f
Christopher Sullivan <S00188845@mail.itsligo.ie>
2020-05-13 10:08:43 +0100

    Properties added

--- Final Exam/Phone.cs
+++ Final Exam/Phone.cs
@@ -12,6 +12,6 @@ namespace Final_Exam
         public int Price { get; set; }
         public string OpSystem{ get; set; }
         public string OsImage{ get; set; }
-        public string  PhoneImage{ get; set; }
+        public string PhoneImage{ get; set; }
     }
 }
